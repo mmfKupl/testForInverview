a79f1eb5334d2ee73a99eb556f8e600e7cba262d
Ivan Kuplevich <ikuplevich97@gmail.com>
2023-09-25 15:58:25 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const orders = [
+  { orderNumber: 10102 },
+  { orderNumber: 10103 },
+  { orderNumber: 10104 },
+  { orderNumber: 10105 },
+  { orderNumber: 10106 }
+];
+
+// const resultArray = [
+//   { orderNumber: 10102 },
+//   { orderNumber: 10106 },
+//   { orderNumber: 10103 },
+//   { orderNumber: 10104 },
+//   { orderNumber: 10105 }
+// ];
+
+const orderDetails = [
+  { orderNumber: 10102, amount: 39, priceEach: "95.55" },
+  { orderNumber: 10102, amount: 41, priceEach: "43.13" },
+  { orderNumber: 10103, amount: 26, priceEach: "214.30" },
+  { orderNumber: 10103, priceEach: "119.67" },
+  { orderNumber: 10103, amount: 27, priceEach: "121.64" },
+  { orderNumber: 10103, priceEach: "94.50" },
+  { orderNumber: 10103, amount: 22, priceEach: "58.34" },
+  { orderNumber: 10103, priceEach: "92.19" },
+  { orderNumber: 10104, amount: 34, priceEach: "131.44" },
+  { orderNumber: 10104, amount: 41, priceEach: "111.39" },
+  { orderNumber: 10104, priceEach: "135.90" },
+  { orderNumber: 10104, amount: 29, priceEach: "122.73" },
+  { orderNumber: 10104, amount: 23, priceEach: "165.95" },
+  { orderNumber: 10105, amount: 50, priceEach: "127.84" },
+  { orderNumber: 10105, amount: 41, priceEach: "205.72" },
+  { orderNumber: 10105, amount: 29, priceEach: "141.88" },
+  { orderNumber: 10105, amount: 22, priceEach: "136.59" },
+  { orderNumber: 10106, amount: 36, priceEach: "134.04" },
+  { orderNumber: 10106, priceEach: "81.10" },
+  { orderNumber: 10106, amount: 41, priceEach: "80.86" }
+];
